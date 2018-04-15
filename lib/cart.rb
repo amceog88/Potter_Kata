6be3ad5c9aac6d7e82724f9b9a59a559feb838d5
Siegmund <amceog88@gmail.com>
2018-04-15 11:36:43 +0800

    finish homework Q1

---
+++ lib/cart.rb
@@ -0,0 +1,31 @@
+class Cart
+
+  PRICE = 100     # 每本書的單價是 100 元
+
+  def add(items)
+    @ordered_items = items.map{ |key,value| value }.delete_if{ |i| i == 0 }  # 把資料由 Hash 轉成陣列
+  end
+
+  def calculate
+    total = 0
+
+    while @ordered_items.size > 0         # 當客戶還有書籍尚未結帳時
+      if @ordered_items.size == 2
+        total = total + 2 * PRICE * 0.95  # 客戶買了一組哈利波特
+      elsif @ordered_items.size == 3
+        total = total + 3 * PRICE * 0.9
+      elsif @ordered_items.size == 4
+        total = total + 4 * PRICE * 0.85
+      elsif @ordered_items.size == 5
+        total = total + 5 * PRICE * 0.8
+      else
+        total = total + PRICE             # 客戶買了單一本哈利波特
+      end
+      @ordered_items = @ordered_items.map{ |i| i -= 1 } # 刪除已計算過的書籍
+      @ordered_items = @ordered_items.delete_if{ |i| i == 0 } # 把已經結完的集數刪除
+    end
+
+    return total
+  end
+
+end
